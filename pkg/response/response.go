f5050e014909894c724d2c7c606d45627763c09d
lyhapple <cnpython@163.com>
2023-09-01 17:18:54 +0800

    update

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -5,32 +5,33 @@ import (
 	"net/http"
 )
 
-func jsonResp(c *gin.Context, code int, extends map[string]interface{}) {
+func EmptyJsonResp(c *gin.Context, code int) {
 	data := gin.H{
 		"code": code,
 		"msg":  GetCodeMsg(code),
-	}
-	if extends != nil {
-		for key, value := range extends {
-			data[key] = value
-		}
 	}
 	c.JSON(http.StatusOK, data)
 }
 
 func SuccessJsonResp(c *gin.Context, result interface{}, extends map[string]interface{}) {
+	ret := gin.H{
+		"code": SuccessCode,
+		"msg":  GetCodeMsg(SuccessCode),
+		"data": result,
+	}
 	if extends != nil {
-		extends["data"] = result
-	} else if result != nil {
-		extends = map[string]interface{}{
-			"data": result,
+		for key, value := range extends {
+			ret[key] = value
 		}
 	}
-	jsonResp(c, SuccessCode, extends)
+	c.JSON(http.StatusOK, ret)
 }
 
 func FailJsonResp(c *gin.Context, code int, err interface{}) {
-	jsonResp(c, code, map[string]interface{}{
+	data := gin.H{
+		"code":  code,
+		"msg":   GetCodeMsg(code),
 		"error": err,
-	})
+	}
+	c.JSON(http.StatusOK, data)
 }
