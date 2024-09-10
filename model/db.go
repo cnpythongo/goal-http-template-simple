5d4ccf902417a69a3213f0a7066c866beea88f2e
cnpythongo <cnpython@163.com>
2024-09-10 16:23:52 +0800

    update

--- model/db.go
+++ model/db.go
@@ -14,8 +14,8 @@ var db *gorm.DB
 
 type BaseModel struct {
 	ID        int64 `gorm:"primary_key;comment:流水ID" json:"-"`
-	CreatedAt int64 `gorm:"column:created_at;autoCreateTime;default:0;comment:数据创建时间" json:"-"`
-	UpdatedAt int64 `gorm:"column:updated_at;autoUpdateTime;default:0;comment:数据更新时间" json:"-"`
+	CreatedAt int64 `gorm:"column:created_at;autoCreateTime;not null;comment:数据创建时间" json:"-"`
+	UpdatedAt int64 `gorm:"column:updated_at;autoUpdateTime;not null;comment:数据更新时间" json:"-"`
 	DeletedAt int64 `gorm:"column:deleted_at;default:0;comment:数据删除时间" json:"-"`
 }
 
